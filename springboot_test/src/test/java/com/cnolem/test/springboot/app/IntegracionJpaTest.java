1e8cba3bb75a5c825366ae9e3f558533369075a8
carnol1989 <carnol1989@hotmail.com>
2023-10-05 23:18:26 -0500

    chore: Section 5-Spring Boot-DataJpaTest-findById, findByPerson, findThrow, findAll

--- springboot_test/src/test/java/com/cnolem/test/springboot/app/IntegracionJpaTest.java
+++ springboot_test/src/test/java/com/cnolem/test/springboot/app/IntegracionJpaTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,5 +24,31 @@ public class IntegracionJpaTest {
 
         assertTrue(cuenta.isPresent());
         assertEquals("Andrés", cuenta.orElseThrow().getPersona());
+    }
+
+    @Test
+    void testFindByPersona() {
+        Optional<Cuenta> cuenta = cuentaRepository.findByPersona("Andrés");
+
+        assertTrue(cuenta.isPresent());
+        assertEquals("Andrés", cuenta.orElseThrow().getPersona());
+        assertEquals("1000.00", cuenta.orElseThrow().getSaldo().toPlainString());
+    }
+
+    @Test
+    void testFindByPersonaThrowException() {
+        Optional<Cuenta> cuenta = cuentaRepository.findByPersona("Rod");
+
+//        assertThrows(NoSuchElementException.class, () -> cuenta.orElseThrow());
+        assertThrows(NoSuchElementException.class, cuenta::orElseThrow);
+        assertFalse(cuenta.isPresent());
+    }
+
+    @Test
+    void testFindAll() {
+        List<Cuenta> cuentas = cuentaRepository.findAll();
+
+        assertFalse(cuentas.isEmpty());
+        assertEquals(2, cuentas.size());
     }
 }
--- springboot_test/src/test/resources/import.sql
+++ springboot_test/src/test/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO cuentas(persona, saldo) VALUES ("Andrés", 1000);
-INSERT INTO cuentas(persona, saldo) VALUES ("Jhon", 2000);
+INSERT INTO cuentas(persona, saldo) VALUES ('Andrés', 1000);
+INSERT INTO cuentas(persona, saldo) VALUES ('Jhon', 2000);
 
-INSERT INTO bancos(nombre, total_transferencias) VALUES ("El Banco Financiero", 0);
+INSERT INTO bancos(nombre, total_transferencias) VALUES ('El Banco Financiero', 0);
