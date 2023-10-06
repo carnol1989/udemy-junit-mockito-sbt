f0d020f672e0598eaba4b1282b33ec00f03caa5b
carnol1989 <carnol1989@hotmail.com>
2023-10-05 21:08:55 -0500

    chore: Section 5-Spring Boot-DataJpaTest-@DataJpaTest 1

---
+++ springboot_test/src/test/java/com/cnolem/test/springboot/app/IntegracionJpaTest.java
@@ -0,0 +1,26 @@
+package com.cnolem.test.springboot.app;
+
+import com.cnolem.test.springboot.app.models.Cuenta;
+import com.cnolem.test.springboot.app.repositories.CuentaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class IntegracionJpaTest {
+
+    @Autowired
+    CuentaRepository cuentaRepository;
+
+    @Test
+    void testFindById() {
+        Optional<Cuenta> cuenta = cuentaRepository.findById(1L);
+
+        assertTrue(cuenta.isPresent());
+        assertEquals("Andrés", cuenta.orElseThrow().getPersona());
+    }
+}
