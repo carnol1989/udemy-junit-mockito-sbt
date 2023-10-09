157099071030a2733eeafd9acdbd50d56593698c
carnol1989 <carnol1989@hotmail.com>
2023-10-09 05:42:59 -0500

    chore: Section 7-Spring Boot: Configure and PI (transfer) with WebTestClient

---
+++ springboot_test/src/test/java/com/cnolem/test/springboot/app/controllers/CuentaControllerWebClientTest.java
@@ -0,0 +1,85 @@
+package com.cnolem.test.springboot.app.controllers;
+
+import com.cnolem.test.springboot.app.models.TransaccionDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class CuentaControllerWebClientTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void testTransferir() throws JsonProcessingException {
+        //Given
+        TransaccionDto dto = new TransaccionDto();
+        dto.setCuentaOrigenId(1L);
+        dto.setCuentaDestinoId(2L);
+        dto.setBancoId(1L);
+        dto.setMonto(new BigDecimal("100"));
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", "ok");
+        response.put("mensaje", "Transferencia realizada con éxito!");
+        response.put("transaccion", dto);
+        System.out.println(objectMapper.writeValueAsString(response));
+
+        //When
+        client.post().uri("/api/cuentas/transferir")//http://localhost:8080/api/cuentas/transferir
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(dto)
+                .exchange()
+        //Then
+                .expectStatus().isOk()
+                .expectBody()
+//                .expectBody(String.class)
+                .consumeWith(respuesta -> {
+                    try {
+//                        String jsonStr = respuesta.getResponseBody();
+//                        JsonNode json = objectMapper.readTree(jsonStr);
+                        JsonNode json = objectMapper.readTree(respuesta.getResponseBody());
+                        assertEquals("Transferencia realizada con éxito!", json.path("mensaje").asText());
+                        assertEquals(1L, json.path("transaccion").path("cuentaOrigenId").asLong());
+                        assertEquals(LocalDate.now().toString(), json.path("date").asText());
+                        assertEquals("100", json.path("transaccion").path("monto").asText());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                //jsonPath requiere expectBody como byte
+                .jsonPath("$.mensaje").isNotEmpty()
+                .jsonPath("$.mensaje").value(is("Transferencia realizada con éxito!"))
+                .jsonPath("$.mensaje").value(valor -> assertEquals("Transferencia realizada con éxito!", valor))
+                .jsonPath("$.mensaje").isEqualTo("Transferencia realizada con éxito!")
+                .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(dto.getCuentaOrigenId())
+                .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
+                .json(objectMapper.writeValueAsString(response));
+    }
+}
