f26fbaa8f2cb5f2a04730e0695a0e77eaf793c5a
carnol1989 <carnol1989@hotmail.com>
2023-10-09 06:05:23 -0500

    chore: Section 7-Spring Boot: PI for detail

--- springboot_test/src/test/java/com/cnolem/test/springboot/app/controllers/CuentaControllerWebClientTest.java
+++ springboot_test/src/test/java/com/cnolem/test/springboot/app/controllers/CuentaControllerWebClientTest.java
@@ -1,11 +1,11 @@
 package com.cnolem.test.springboot.app.controllers;
 
+import com.cnolem.test.springboot.app.models.Cuenta;
 import com.cnolem.test.springboot.app.models.TransaccionDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -22,6 +22,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class CuentaControllerWebClientTest {
 
@@ -36,6 +37,7 @@ class CuentaControllerWebClientTest {
     }
 
     @Test
+    @Order(1)
     void testTransferir() throws JsonProcessingException {
         //Given
         TransaccionDto dto = new TransaccionDto();
@@ -56,8 +58,9 @@ class CuentaControllerWebClientTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(dto)
                 .exchange()
-        //Then
+                //Then
                 .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBody()
 //                .expectBody(String.class)
                 .consumeWith(respuesta -> {
@@ -81,5 +84,33 @@ class CuentaControllerWebClientTest {
                 .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(dto.getCuentaOrigenId())
                 .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
                 .json(objectMapper.writeValueAsString(response));
+    }
+
+    @Test
+    @Order(2)
+    void testDetalle() throws JsonProcessingException {
+        Cuenta cuenta = new Cuenta(1L, "Andrés", new BigDecimal("900"));
+
+        client.get().uri("/api/cuentas/1").exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.persona").isEqualTo("Andrés")
+                .jsonPath("$.saldo").isEqualTo(900)
+                .json(objectMapper.writeValueAsString(cuenta));
+    }
+
+    @Test
+    @Order(3)
+    void testDetalle2() {
+        client.get().uri("/api/cuentas/2").exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(Cuenta.class)
+                .consumeWith(response -> {
+                    Cuenta cuenta = response.getResponseBody();
+                    assertEquals("Jhon", cuenta.getPersona());
+                    assertEquals("2100.00", cuenta.getSaldo().toPlainString());
+                });
     }
 }
