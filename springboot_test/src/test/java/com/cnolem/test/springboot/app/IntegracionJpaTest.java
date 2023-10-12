7e3012b5c1218d5a774b7a176252dc0c7def7c88
carnol1989 <carnol1989@hotmail.com>
2023-10-12 07:13:29 -0500

    feat: Section 8-Add tags and convert encoding file import

--- springboot_test/src/test/java/com/cnolem/test/springboot/app/IntegracionJpaTest.java
+++ springboot_test/src/test/java/com/cnolem/test/springboot/app/IntegracionJpaTest.java
@@ -2,6 +2,7 @@ package com.cnolem.test.springboot.app;
 
 import com.cnolem.test.springboot.app.models.Cuenta;
 import com.cnolem.test.springboot.app.repositories.CuentaRepository;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Tag("integracion_jpa")
 @DataJpaTest
 public class IntegracionJpaTest {
 
